12a25e02fecc306b4db50e339f5ebb7588a7f576
Kim Ng <kimleeng@gmail.com>
2017-12-01 15:25:27 +0100

    utilize config

--- fastfilter.py
+++ fastfilter.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import subprocess
 import argparse
+import ruamel.yaml
+import pkg_resources
 
 # for f in ../../cge_dbs/resfinder_db/*.fsa; do (cat "${f}"; echo) >> resfinder.fasta; done
+config_file = pkg_resources.resource_filename(__name__, "config.yaml")
+yaml = ruamel.yaml.YAML(typ='safe')
+yaml.default_flow_style = False
+with open(config_file, "r") as yaml_stream:
+    config = yaml.load(yaml_stream)
 
 
 def program_initialization():
@@ -28,6 +35,10 @@ def program_initialization():
                         help="Returns reads that aren't in this set instead of ones that are",
                         action="store_true",
                         default=False)
+    parser.add_argument("--kaiju",
+                        help="Uses Kaiju a BWT AA approach over Kraken",
+                        action="store_true",
+                        default=False)
     args = parser.parse_args()
 
     return args
@@ -36,14 +47,24 @@ def program_initialization():
 def filter_reads_on_kraken(R1_reads, R2_reads, outfile, db_location, threads, inverse):
     if R2_reads is None:
         R2_reads = ""
+
     if not inverse:
-        subprocess.call("kraken --db {} --threads {} --quick --min-hits 1 --classified-out {} {} {} 1> /dev/null".format(db_location, threads, outfile, R1_reads, R2_reads), shell=True)
+        subprocess.call("kraken --db {} --threads {} {} --classified-out {} {} {} 1> /dev/null".format(db_location, threads, config["kraken"]["options"], outfile, R1_reads, R2_reads), shell=True)
     else:
-        subprocess.call("kraken --db {} --threads {} --quick --min-hits 1 --unclassified-out {} {} {} 1> /dev/null".format(db_location, threads, outfile, R1_reads, R2_reads), shell=True)
+        subprocess.call("kraken --db {} --threads {} {} --unclassified-out {} {} {} 1> /dev/null".format(db_location, threads, config["kraken"]["options"], outfile, R1_reads, R2_reads), shell=True)
+    return 0
+
+
+def filter_reads_on_kaiju(R1_reads, R2_reads, outfile, db_location, threads, inverse):
+    # subprocess.call("kaiju -t {} -f {} -i {} -j {} -z {} {}")
+    return 0
 
 
 if __name__ == "__main__":
     args = program_initialization()
     print("Starting")
-    filter_reads_on_kraken(args.R1_reads, args.R2_reads, args.output_name, args.database_to_use, args.threads, args.inverse)
+    if args.Kaiju:
+        filter_reads_on_kaiju()
+    else:
+        filter_reads_on_kraken(args.R1_reads, args.R2_reads, args.output_name, args.database_to_use, args.threads, args.inverse)
     print("Complete")
