d9fe6110ec1e6af7471f58c23e77b34acbbe9327
Kim Ng <kimleeng@gmail.com>
2017-12-01 16:06:40 +0100

    adding bbnorm and fixing kraken formatiing

--- fastfilter.py
+++ fastfilter.py
@@ -39,13 +39,21 @@ def program_initialization():
                         help="Uses Kaiju a BWT AA approach over Kraken",
                         action="store_true",
                         default=False)
+    parser.add_argument("--norm",
+                        help="Uses bbnorm to normalize reads down to ensure maximum coverage on kmers",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("--config",
+                        help="Config file to overwrite",
+                        type=str,
+                        default="")
     args = parser.parse_args()
 
     return args
 
 
 def filter_reads_on_kraken(R1_reads, R2_reads, outfile, db_location, threads, inverse):
-    paired = "--paired "
+    paired = "--paired --out-fmt paired "
     if R2_reads is None:
         R2_reads = ""
         paired = ""
@@ -54,6 +62,9 @@ def filter_reads_on_kraken(R1_reads, R2_reads, outfile, db_location, threads, in
         subprocess.call("kraken --db {} --threads {} {} {} --classified-out {} {} {} 1> /dev/null".format(db_location, threads, config["kraken"]["options"], paired, outfile, R1_reads, R2_reads), shell=True)
     else:
         subprocess.call("kraken --db {} --threads {} {} {} --unclassified-out {} {} {} 1> /dev/null".format(db_location, threads, config["kraken"]["options"], paired, outfile, R1_reads, R2_reads), shell=True)
+
+    if R2_reads is None:
+        return (outfile)
     return 0
 
 
@@ -62,11 +73,27 @@ def filter_reads_on_kaiju(R1_reads, R2_reads, outfile, db_location, threads, inv
     return 0
 
 
+def bbnorm_results(R1_reads, R2_reads, threads):
+    R1_normalized = "norm" + R1_reads
+    R2_normalized = "norm" + R2_reads
+    if R2_reads is None:
+        subprocess.call("bbnorm.sh threads={} in={} out={} {}".format(threads, R1_reads, R1_normalized, config["bbnorm"]["options"]))
+    else:
+        subprocess.call("bbnorm.sh threads={} in={} in2={} out={} out2={} {}".format(threads, R1_reads, R2_reads, R1_normalized, R2_normalized, config["bbnorm"]["options"]))
+
+
 if __name__ == "__main__":
     args = program_initialization()
     print("Starting")
+
+    if args.config != "":
+        with open(args.config, "r") as yaml_stream:
+            config = yaml.load(yaml_stream)
+
     if args.kaiju:
         filter_reads_on_kaiju()
     else:
         filter_reads_on_kraken(args.R1_reads, args.R2_reads, args.output_name, args.database_to_use, args.threads, args.inverse)
+    if args.bbnorm:
+        bbnorm_results(args.output_name + "_R1.fastq", args.output_name + "_R2.fastq", args.threads)
     print("Complete")
