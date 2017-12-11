7012c103b0377c09452f3febe1afd9864d49a4d0
Kim Ng <kimleeng@gmail.com>
2017-12-11 12:27:25 +0100

    added in kaiju

--- fastfilter.py
+++ fastfilter.py
@@ -3,6 +3,8 @@ import subprocess
 import argparse
 import ruamel.yaml
 import pkg_resources
+import skbio
+import gzip
 
 # for f in ../../cge_dbs/resfinder_db/*.fsa; do (cat "${f}"; echo) >> resfinder.fasta; done
 config_file = pkg_resources.resource_filename(__name__, "config.yaml")
@@ -13,9 +15,9 @@ with open(config_file, "r") as yaml_stream:
 
 
 def program_initialization():
-    parser = argparse.ArgumentParser(description='Fastfilter - makedb, generate a DB to be used for filtering of reads')
+    parser = argparse.ArgumentParser(description='Fastfilter - filter a set of reads against a database (default: kraken)')
     parser.add_argument("-db", "--database_to_use",
-                        help="Create a kraken db at location",
+                        help="db at location",
                         type=str,
                         required=True)
     parser.add_argument("-R1", "--R1_reads",
@@ -69,7 +71,35 @@ def filter_reads_on_kraken(R1_reads, R2_reads, outfile, db_location, threads, in
 
 
 def filter_reads_on_kaiju(R1_reads, R2_reads, outfile, db_location, threads, inverse):
-    # subprocess.call("kaiju -t {} -f {} -i {} -j {} -z {} {}")
+    # subprocess.call("kaijux -t {} -f {} -i {} -j {} -z {} {}")
+    tempfile = "kaiju.out"
+    if R2_reads is None:
+        subprocess.call("kaijux -z {} -f {} -i {} -o {} {}".format(threads, db_location, R1_reads, tempfile, config["kaiju"]["options"]), shell=True)
+    else:
+        subprocess.call("kaijux -z {} -f {} -i {} -j {} -o {} {}".format(threads, db_location, R1_reads, R2_reads, tempfile, config["kaiju"]["options"]), shell=True)
+
+    if not inverse:
+        extract_reads(tempfile, R1_reads, R2_reads, "C", outfile)
+    else:
+        extract_reads(tempfile, R1_reads, R2_reads, "U", outfile)
+    return 0
+
+
+def extract_reads(classifier_file, R1_reads, R2_reads, classification_symbol, outfile):
+    read_dict = {}
+    with open(classifier_file, "r") as classifier:
+        for line in classifier:
+            if line.startswith(classification_symbol):
+                read_dict[line.split("\t")[1]] = ""
+    with open(outfile + "_R1.fastq") as R1_out:
+        for seq in skbio.io.read(R1_reads, format="fastq"):
+            if seq.metadata['id'] in read_dict:
+                seq.write(R1_out)
+    if R2_reads is not None:
+        with open(outfile + "_R2.fastq") as R2_out:
+            for seq in skbio.io.read(R2_reads, format="fastq"):
+                if seq.metadata['id'] in read_dict:
+                    seq.write(R2_out)
     return 0
 
 
@@ -80,6 +110,7 @@ def bbnorm_results(R1_reads, R2_reads, threads):
         subprocess.call("bbnorm.sh threads={} in={} out={} {} 1> /dev/null".format(threads, R1_reads, R1_normalized, config["bbnorm"]["options"]), shell=True)
     else:
         subprocess.call("bbnorm.sh threads={} in={} in2={} out={} out2={} {} 1> /dev/null".format(threads, R1_reads, R2_reads, R1_normalized, R2_normalized, config["bbnorm"]["options"]), shell=True)
+    return 0
 
 
 if __name__ == "__main__":
@@ -99,4 +130,5 @@ if __name__ == "__main__":
             bbnorm_results(args.output_name + "_R1.fastq", args.R2_reads, args.threads)
         else:
             bbnorm_results(args.output_name + "_R1.fastq", args.output_name + "_R2.fastq", args.threads)
+
     print("Complete")
