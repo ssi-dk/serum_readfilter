888efae67430ed9a73e3ff612f5284d55c50bd68
Kim Ng <kimleeng@gmail.com>
2017-12-19 14:14:37 +0100

    working on packaging still

--- fastfilter/__init__.py
+++ fastfilter/__init__.py
@@ -1,11 +1,3 @@
-from pkg_resources import get_distribution
-
-try:
-    __version__ = get_distribution('fastfilter').version
-except:
-    __version__ = 'local'
-
-
 __all__ = [
     'makedb',
     'runfilter'
--- fastfilter/makedb.py
+++ fastfilter/makedb.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import Bio.SeqIO
 import os
 import subprocess
