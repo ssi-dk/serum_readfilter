6e8f2e2aedd56ae4b87e77b216f2ac41fe6bcff1
Kim Ng <kimleeng@gmail.com>
2017-12-22 11:25:05 +0100

    debug

--- fastfilter/tests/test_kraken.py
+++ fastfilter/tests/test_kraken.py
@@ -13,17 +13,18 @@ class fastfilterTests(unittest.TestCase):
 
     def test_make_db(self):
         makedb.make_kraken_db_from_fasta("data/cdifficile_mlst", "cdifficile_db", 1, 31, ["tfa"])
-        # self.assertTrue(os.path.isdir("cdifficile_db"))
+        self.assertTrue(os.path.isdir("cdifficile_db"))
+        self.assertTrue(os.path.isfile("cdifficile_db/lca.complete"))
 
     def test_runfilter(self):
         runfilter.filter_reads_on_kraken("data/reads/cdifficile_R1.fastq.gz", "data/reads/cdifficile_R1.fastq.gz", "filtered", "cdifficile_db", 1, False)
-        # self.assertTrue(os.path.isfile("filtered_R1.fastq"))
-        # self.assertTrue(os.path.isfile("filtered_R2.fastq"))
+        self.assertTrue(os.path.isfile("filtered_R1.fastq"))
+        self.assertTrue(os.path.isfile("filtered_R2.fastq"))
 
     def tearDown(self):
-        # shutil.rmtree("cdifficile_db")
-        # os.remove("filtered_R1.fastq")
-        # os.remove("filtered_R2.fastq")
+        shutil.rmtree("cdifficile_db")
+        os.remove("filtered_R1.fastq")
+        os.remove("filtered_R2.fastq")
         pass
 
 
