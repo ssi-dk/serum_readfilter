f6d1f871a770275a018e4fb10c6d1fbe781c92fc
Kim Ng <kimleeng@gmail.com>
2017-12-22 11:39:14 +0100

    debug

--- fastfilter/tests/test_kraken.py
+++ fastfilter/tests/test_kraken.py
@@ -1,6 +1,7 @@
 import unittest
 import shutil
 import os
+import tempfile
 
 from fastfilter import makedb
 from fastfilter import runfilter
@@ -9,22 +10,26 @@ from fastfilter import runfilter
 class fastfilterTests(unittest.TestCase):
 
     def setUp(self):
+        self.here = os.path.abspath(__file__)
+        self.test_dir = tempfile.mkdtemp()
+        os.chdir(self.test_dir)
         pass
 
     def test_make_db(self):
-        makedb.make_kraken_db_from_fasta("data/cdifficile_mlst", "cdifficile_db", 1, 31, ["tfa"])
+        makedb.make_kraken_db_from_fasta(os.path.join(self.here, "/data/cdifficile_mlst"), "cdifficile_db", 1, 31, ["tfa"])
         self.assertTrue(os.path.isdir("cdifficile_db"))
         self.assertTrue(os.path.isfile("cdifficile_db/lca.complete"))
 
     def test_runfilter(self):
-        runfilter.filter_reads_on_kraken("data/reads/cdifficile_R1.fastq.gz", "data/reads/cdifficile_R1.fastq.gz", "filtered", "cdifficile_db", 1, False)
+        runfilter.filter_reads_on_kraken(os.path.join(self.here, "data/reads/cdifficile_R1.fastq.gz"), os.path.join(self.here, "data/reads/cdifficile_R1.fastq.gz"), "filtered", "cdifficile_db", 1, False)
         self.assertTrue(os.path.isfile("filtered_R1.fastq"))
         self.assertTrue(os.path.isfile("filtered_R2.fastq"))
 
     def tearDown(self):
-        shutil.rmtree("cdifficile_db")
         os.remove("filtered_R1.fastq")
         os.remove("filtered_R2.fastq")
+        os.chdir(self.here)
+        shutil.rmtree("cdifficile_db")
         pass
 
 
